5fdf8528a0edc074729bd8f092a1342c20a517b3
ideal1st.here@googlemail.com <ideal1st.here@googlemail.com>
2023-09-06 13:27:24 +0700

    Updated Config and 10 other files

--- home.markdown
+++ home.markdown
@@ -1,7 +1,6 @@
 ---
-title: Pesantren Kampung Santri
-date: 2023-09-05 13:31:00 +07:00
-alias: Beranda
-layout: beranda
-bgimg: "/uploads/psb2.png"
+title: Home
+date: 2023-08-27 23:50:00 +07:00
+layout: home
 ---
+
--- js/agency.js
+++ js/agency.js
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Start Bootstrap - Agnecy Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
@@ -21,7 +21,7 @@ $('body').scrollspy({
 })
 
 // Closes the Responsive Menu on Menu Item Click
-$('ul.dropdown-menu li a').click(function() {
+$('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
